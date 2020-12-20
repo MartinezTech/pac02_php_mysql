6e54d5cdb792529dd9548d40b97b7080bd5f67c8
MartinezTech <alexmartinez.mm98@gmail.com>
2020-12-20 23:29:23 +0100

    add comments

---
+++ N3P206alexmartinez_comments.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments N3P1</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Comments for PAC02 - mysql and php</h1>
+        
+        <h3>List of learn concepts</h3>
+        <ol>
+            <li>How to create mysql databases with php</li>
+            <li>How to insert data into mysql databases with php</li>
+            <li>How to generate mysql connection mysql with php</li>
+            <li>How to create foreign keys using alter table with php </li>
+            <li>How to show rows of databases with php</li>
+            <li>How to do queries in databases with php</li>
+            <li>How to insert data of mysql database into html tables</li>
+            <li>How use inner join in mysql</li>
+            <li>How to use alias in mysql</li>
+            <li>How show the query results in different pages</li>
+        </ol>
+        <h3>Document and teacher explanation</h3>
+        <p>I think it's great.
+        </p>
+        <h3>Mark</h3>
+        <p>
+            I don't like mark myself. I always try to do my best.
+        </p>
+        <h3>Improvements</h3>
+        <p>I don't see any improvement. It seems fine.</p>
+    </div>
+    
+</body>
+</html>
